09785aaad637e224640d4422a3a1dc9613b28957
Mrbin678 <1140465913@qq.com>
2018-11-20 21:28:14 +0800

    1111

---
+++ src/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Create by wangbin
+ * 2018-11-20-16:16
+ */
+//快速生成main()的方法,直接输入 psvm 就会有提示
+//快速生成system.out 的方法，sout 就会有提示
+// 快速格式化代码的快捷键：ctrl+alt+L
+//try-catch的快捷键 ：在需要包裹的代码所在行输入，ctrl+alt+t ,选择try catch选项即可
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
